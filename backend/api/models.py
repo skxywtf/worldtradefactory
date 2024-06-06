f48b46ec0057c75465790ff9a4ab458868b38fa8
mangesh vispute <mangesh2003vispute@gmail.com>
2024-06-06 17:22:16 +0530

    Updated .gitignore to ignore .env file

--- backend/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-# Additional custom fields will go here. 
-# Uncomment the: AUTH_USER_MODEL = 'yourapp.CustomUser' in settings.py
-# register in the admin.py
-# makemigartions and then migrate
-
-class CustomUser(AbstractUser):
-    pass
