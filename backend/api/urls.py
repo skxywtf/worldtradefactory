abf3ad95b156968cd55ab5c24fa3d89760f1d7d2
Aman Raj <compidemail@gmail.com>
2024-06-12 11:11:38 +0530

    final contact us api is done

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,5 +8,5 @@ from .views import ContactCreateView
 urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('contact/', ContactCreateView.as_view(), name='contact-create'),
+    path('contact/', ContactCreateView.as_view(), name='contact-create'), #done
 ]
