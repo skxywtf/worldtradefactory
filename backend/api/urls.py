224ea745524fbc80a77f4d7a6e5230d9b6b9d12b
Aman Raj <compidemail@gmail.com>
2024-07-22 23:30:15 +0530

    technical indicators pushed

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -25,7 +25,15 @@ from .views import (
     load_treasury_yield,load_unemployment_rate,load_historical_options,
     load_balance_sheet,load_cash_flow,load_dividends,load_earnings,
     load_income_statement,load_overview,load_splits,
-    load_stock_gl,load_stock_news
+    load_stock_gl,load_stock_news,load_stock_analytics,
+    load_ad,load_adosc,load_adx,load_adxr,load_apo,load_aroon,load_aroonosc,
+    load_atr,load_bbands,load_bop,load_cci,load_cmo,load_dcperiod,load_dcphase,
+    load_dcphasor,load_dema,load_dx,load_ema,load_kama,load_macdext,load_mama,
+    load_mfi,load_midpoint,load_midprice,load_minus_di,load_minus_dm,load_mom,
+    load_natr,load_obv,load_plus_di,load_plus_dm,load_ppo,load_roc,load_rocr,
+    load_rsi,load_sar,load_sine,load_sma,load_stoch,load_stochf,load_stochrsi,
+    load_tema,load_trange,load_trendline,load_trendmode,load_trima,load_trix,load_tt,
+    load_ultosc,load_willr,load_wma
 )
 
 urlpatterns = [
@@ -36,7 +44,7 @@ urlpatterns = [
     path('listai/', ImageUploadView.as_view(), name='listai'), #done
 
     # for 3rd-party data saving into our db
-    path('fetch-country-data/', fetch_country_data, name='fetch_country_data'),
+    #path('fetch-country-data/', fetch_country_data, name='fetch_country_data'),
     #path('fetch-economic-data/', fetch_economic_data, name='fetch_economic_data'),
     #path('fetch-education-data/', fetch_education_data, name='fetch_education_data'),
     #path('fetch-employment-data/', fetch_employment_data, name='fetch_employment_data'),
@@ -136,6 +144,58 @@ urlpatterns = [
     # Alpha-Intelligence
     path('load-news/<str:symbol>', load_stock_news, name='load_stock_news'),
     path('load-gl', load_stock_gl, name='gain_loss'),
-    #path('load-analytics', load_stock_analytics, name='analytics_data'),
+    path('load-analytics', load_stock_analytics, name='analytics_data'), # TWO TICKERS AT A TIME TO FETCH
+
+    # Technical indicators alpha vantage
+    path('load-sma/<str:symbol>', load_sma, name='load_sma'),
+    path('load-ema/<str:symbol>', load_ema, name='load_ema'),
+    path('load-wma/<str:symbol>', load_wma, name='load_wma'),
+    path('load-dema/<str:symbol>', load_dema, name='load_dema'),
+    path('load-tema/<str:symbol>', load_tema, name='load_tema'),
+    path('load-trima/<str:symbol>', load_trima, name='load_trima'),
+    path('load-kama/<str:symbol>', load_kama, name='load_kama'),
+    path('load-mama/<str:symbol>', load_mama, name='load_mama'),
+    path('load-tt/<str:symbol>', load_tt, name='load_tt'),
+    path('load-macdext/<str:symbol>', load_macdext, name='load_macdext'),
+    path('load-stoch/<str:symbol>', load_stoch, name='load_stoch'),
+    path('load-stochf/<str:symbol>', load_stochf, name='load_stochf'),
+    path('load-rsi/<str:symbol>', load_rsi, name='load_rsi'),
+    path('load-stochrsi/<str:symbol>', load_stochrsi, name='load_stochrsi'),
+    path('load-willr/<str:symbol>', load_willr, name='load_willr'),
+    path('load-adx/<str:symbol>', load_adx, name='load_adx'),
+    path('load-adxr/<str:symbol>', load_adxr, name='load_adxr'),
+    path('load-apo/<str:symbol>', load_apo, name='load_apo'),
+    path('load-ppo/<str:symbol>', load_ppo, name='load_ppo'),
+    path('load-mom/<str:symbol>', load_mom, name='load_mom'),
+    path('load-bop/<str:symbol>', load_bop, name='load_bop'),
+    path('load-cci/<str:symbol>', load_cci, name='load_cci'),
+    path('load-cmo/<str:symbol>', load_cmo, name='load_cmo'),
+    path('load-roc/<str:symbol>', load_roc, name='load_roc'),
+    path('load-rocr/<str:symbol>', load_rocr, name='load_rocr'),
+    path('load-aroon/<str:symbol>', load_aroon, name='load_aroon'),
+    path('load-aroonosc/<str:symbol>', load_aroonosc, name='load_aroonosc'),
+    path('load-mfi/<str:symbol>', load_mfi, name='load_mfi'),
+    path('load-trix/<str:symbol>', load_trix, name='load_trix'),
+    path('load-ultosc/<str:symbol>', load_ultosc, name='load_ultosc'),
+    path('load-minusdi/<str:symbol>', load_minus_di, name='load_minus_di'),
+    path('load-plusdi/<str:symbol>', load_plus_di, name='load_plus_di'),
+    path('load-minusdm/<str:symbol>', load_minus_dm, name='load_minus_dm'),
+    path('load-plusdm/<str:symbol>', load_plus_dm, name='load_plus_dm'),
+    path('load-bbands/<str:symbol>', load_bbands, name='load_bbands'),
+    path('load-midpoint/<str:symbol>', load_midpoint, name='load_midpoint'),
+    path('load-midprice/<str:symbol>', load_midprice, name='load_midprice'),
+    path('load-sar/<str:symbol>', load_sar, name='load_sar'),
+    path('load-trange/<str:symbol>', load_trange, name='load_trange'),
+    path('load-atr/<str:symbol>', load_atr, name='load_atr'),
+    path('load-natr/<str:symbol>', load_natr, name='load_natr'),
+    path('load-ad/<str:symbol>', load_ad, name='load_ad'),
+    path('load-adosc/<str:symbol>', load_adosc, name='load_adosc'),
+    path('load-obv/<str:symbol>', load_obv, name='load_obv'),
+    path('load-trendline/<str:symbol>', load_trendline, name='load_trendline'),
+    path('load-sine/<str:symbol>', load_sine, name='load_sine'),
+    path('load-trendmode/<str:symbol>', load_trendmode, name='load_trendmode'),
+    path('load-dcperiod/<str:symbol>', load_dcperiod, name='load_dcperiod'),
+    path('load-dcphase/<str:symbol>', load_dcphase, name='load_dcphase'),
+    path('load-dcphasor/<str:symbol>', load_dcphasor, name='load_dcphasor'),
 ]
 
