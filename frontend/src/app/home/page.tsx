16b999b89b3ede174985f9f1d8c7799821cbdfff
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-10-27 18:15:11 +0530

    feat: new landing page

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -2,16 +2,16 @@
 import { SetStateAction, useState } from "react";
 import LandingPage from "@/components/landing page/LandingPage";
 export default function Home() {
-  const [searchInput, setSearchInput] = useState("");
+  // const [searchInput, setSearchInput] = useState("");
 
-  const handleSearchSubmit = (input: SetStateAction<string>) => {
-    setSearchInput(input);
-  };
-  const [theme, setTheme] = useState("light");
+  // const handleSearchSubmit = (input: SetStateAction<string>) => {
+  //   setSearchInput(input);
+  // };
+  // const [theme, setTheme] = useState("light");
 
-  const handleTheme = (color: SetStateAction<string>) => {
-    setTheme(color);
-  };
+  // const handleTheme = (color: SetStateAction<string>) => {
+  //   setTheme(color);
+  // };
 
   return (
     <main>
--- frontend/src/components/landing page/LandingPage.tsx
+++ frontend/src/components/landing page/LandingPage.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import HeaderLand from "./header footer landing/HeaderLand";
 import TickerTape from "../embeded codes/other widgets/TickerTape";
 import MainCOntentLand from "./ContentLand Page/MainCOntentLand";
 import FooterLand from "./header footer landing/FooterLand";
 import { useTheme } from 'next-themes'
+
 const LandingPage = () => {
   const { theme  } = useTheme(); // Get the current theme and toggle function
 
