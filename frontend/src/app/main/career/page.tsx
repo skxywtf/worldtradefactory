bd1dfbb58cb879fdd49f448db7c56bdd053a965d
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-12-08 23:10:50 +0530

    career page defined

---
+++ frontend/src/app/main/career/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { SidebarDemo1 } from "@/components/header and Footer/Career/Career_A";
+import Contactn from "@/components/header and Footer/Career/Career_header";
+import Footer from "@/components/header and Footer/Footer";
+
+export default function AboutPage() {
+  return (
+    <>
+      <SidebarDemo1></SidebarDemo1>
+
+      <Footer></Footer>
+    </>
+  );
+}
