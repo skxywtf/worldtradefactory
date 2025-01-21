9e90fc3a96e11e2561a3d0664b7b899ed823c4f5
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2025-01-21 14:38:29 +0530

    refactor: Home component for improved structure and functionality

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,11 +1,36 @@
-"use client";
-import LandingPage from "@/components/landing page/LandingPage";
+"use client"
+import EconomicCalendar from "@/components/embeded codes/Advance widgets/EconomicCalendar";
+import TopStories from "@/components/embeded codes/Advance widgets/TopStories";
+import ETFHeatMap from "@/components/embeded codes/HeatMaps/ETFHeatMap";
+import StockHeatMap from "@/components/embeded codes/HeatMaps/StockHeatMap";
+import MarketData from "@/components/embeded codes/other widgets/MarketData";
+import TickerTape from "@/components/embeded codes/other widgets/TickerTape";
+import GetStarted from "@/components/landing page/ContentLand Page/GetStarted";
+import FooterLand from "@/components/landing page/header footer landing/FooterLand";
+import HeaderLand from "@/components/landing page/header footer landing/HeaderLand";
 
 export default function Home() {
+    return(
+        <div className="w-full">
+            <HeaderLand />
+            <TickerTape />
 
-  return (
-    <main>
-     <LandingPage />
-    </main>
-  );
+            <div className="flex flex-col justify-center items-center gap-4 md:flex-row md:items-start">
+            <MarketData />
+            <TopStories />
+            <EconomicCalendar />
+            </div>
+
+            <div className="h-screen p-10 pt-0">
+            <StockHeatMap />
+            </div>
+            <div className="h-screen px-10">
+            <ETFHeatMap />
+            </div>
+
+            <GetStarted />
+
+            <FooterLand />
+        </div>
+    )
 }
